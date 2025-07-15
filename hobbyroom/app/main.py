f016b830803d031e6d1f33cc4c5878ddac506aca
Changmin Lucas Lee <117613355+lucathree@users.noreply.github.com>
2025-07-15 17:04:21 +0900

    Feature 2: 서버 구동 도커 컨테이너 설정 (#2)
    * update: gitignore 항목 추가
    
    * feat: 헬스체크 엔드포인트 추가
    
    * feat: 서버 실행 도커 컨테이너 환경 설정
    
    * fix: 파이썬 이미지 소스 수정

--- hobbyroom/app/main.py
+++ hobbyroom/app/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+import http
+from fastapi import FastAPI, Response
 
 
 app = FastAPI(
     title="Hobbyroom API",
     description="취미모임 프로젝트 API 서버",
+)
+app.get("/health-check", include_in_schema=False)(
+    lambda: Response(status_code=http.HTTPStatus.OK)
 )
