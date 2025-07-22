1526bd480e9db6fe57209b569e7054eae8decee3
Changmin Lucas Lee <clee0627@gmail.com>
2025-07-22 23:08:16 +0900

    feat: 페르소나 생성 핸들러 추가

--- hobbyroom/user/dependency.py
+++ hobbyroom/user/dependency.py
@@ -8,10 +8,12 @@ class AdapterContainer(containers.DeclarativeContainer):
     session_factory = providers.Dependency()
 
     user_repo_factory = providers.Factory(lambda: adapter.UserRepository)
+    persona_repo_factory = providers.Factory(lambda: adapter.PersonaRepository)
     user_unit_of_work = providers.Factory(
         adapter.UserUnitOfWork,
         session_factory=session_factory,
         user_repo_factory=user_repo_factory,
+        persona_repo_factory=persona_repo_factory,
     )
 
 
@@ -36,6 +38,12 @@ class ServiceContainer(containers.DeclarativeContainer):
         service.AuthorizeUserHandler,
         user_unit_of_work=adapter.user_unit_of_work,
         jwt_handler=jwt_handler,
+    )
+    create_persona_handler = providers.Factory(
+        service.CreatePersonaHandler,
+        user_unit_of_work=adapter.user_unit_of_work,
+        id_generator=id_generator,
+        clock=clock,
     )
 
 
--- hobbyroom/user/service/command_handler.py
+++ hobbyroom/user/service/command_handler.py
@@ -57,3 +57,26 @@ class AuthorizeUserHandler:
         )
         if not is_valid:
             raise exceptions.UnauthorizedError("Invalid password")
+
+
+class CreatePersonaHandler:
+    def __init__(
+        self,
+        user_unit_of_work: adapter.UserUnitOfWork,
+        id_generator: Callable[..., UUID],
+        clock: Callable[..., pendulum.DateTime],
+    ):
+        self.user_unit_of_work = user_unit_of_work
+        self.id_generator = id_generator
+        self.clock = clock
+
+    def handle(self, cmd: command.CreatePersona) -> None:
+        persona = domain.Persona.create(
+            id=self.id_generator(),
+            name=cmd.name,
+            user_id=cmd.user_id,
+            created_at=self.clock(),
+        )
+        with self.user_unit_of_work as uow:
+            uow.persona.add(persona)
+            uow.commit()
