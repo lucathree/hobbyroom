d7a3aa110e663989b2451ca141e110856cddf245
Changmin Lucas Lee <clee0627@gmail.com>
2025-07-22 23:06:50 +0900

    feat: 페르소나 도메인 모델 정의

--- hobbyroom/user/domain/model.py
+++ hobbyroom/user/domain/model.py
@@ -1,8 +1,11 @@
 import datetime
 from typing import Self
+from uuid import UUID
+
 import pendulum
 from pydantic import BaseModel, EmailStr
-from uuid import UUID
+
+from hobbyroom import exceptions
 
 
 class User(BaseModel):
@@ -24,6 +27,34 @@ class User(BaseModel):
             id=id,
             email=email,
             password=hashed_password,
+            created_at=created_at,
+            updated_at=created_at,
+        )
+
+
+class Persona(BaseModel):
+    id: UUID
+    name: str
+    user_id: UUID
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+
+    @classmethod
+    def create(
+        cls,
+        id: UUID,
+        name: str,
+        user_id: UUID,
+        created_at: pendulum.DateTime,
+    ) -> Self:
+        if user_id is None:
+            raise exceptions.DomainValidationError(
+                "페르소나 생성을 위해서는 사용자 ID가 필요합니다."
+            )
+        return cls(
+            id=id,
+            name=name,
+            user_id=user_id,
             created_at=created_at,
             updated_at=created_at,
         )
