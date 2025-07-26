04970f9b0842de03febfd0a2b0af70e398eaafc7
Changmin Lucas Lee <clee0627@gmail.com>
2025-07-26 23:11:11 +0900

    update: 페르소나 id 검증 메소드 추가

--- hobbyroom/gathering/command.py
+++ hobbyroom/gathering/command.py
@@ -2,11 +2,21 @@ from uuid import UUID
 
 from pydantic import BaseModel
 
-from hobbyroom.gathering import domain
+from hobbyroom import exceptions
+from hobbyroom import user as user_domain
 
 
 class CreateGathering(BaseModel):
     name: str
     description: str
     persona_id: UUID
-    user: domain.User | None = None
+
+    def validate_persona_id(self, user: user_domain.User) -> None:
+        persona = next(
+            (persona for persona in user.personas if persona.id == self.persona_id),
+            None,
+        )
+        if persona is None:
+            raise exceptions.NotFoundError(
+                "유효한 페르소나 정보가 사용자에게 없습니다."
+            )
