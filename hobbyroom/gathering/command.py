ee98246d34bcb984c97691e807b3dd7130be67ec
Changmin Lucas Lee <clee0627@gmail.com>
2025-07-28 23:34:53 +0900

    feat: 사용자 모임 참여 커맨드 추가

--- hobbyroom/gathering/command.py
+++ hobbyroom/gathering/command.py
@@ -20,3 +20,18 @@ class CreateGathering(BaseModel):
             raise exceptions.NotFoundError(
                 "유효한 페르소나 정보가 사용자에게 없습니다."
             )
+
+
+class JoinGathering(BaseModel):
+    gathering_id: UUID
+    persona_id: UUID
+
+    def validate_persona_id(self, user: user_domain.User) -> None:
+        persona = next(
+            (persona for persona in user.personas if persona.id == self.persona_id),
+            None,
+        )
+        if persona is None:
+            raise exceptions.NotFoundError(
+                "유효한 페르소나 정보가 사용자에게 없습니다."
+            )
