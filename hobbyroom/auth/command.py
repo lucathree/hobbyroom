db021e6ddae05f26a2c35bc4524dd4dd26c713bb
Changmin Lucas Lee <clee0627@gmail.com>
2025-08-06 00:16:12 +0900

    feat: 페르소나 인증 커맨드 정의

--- hobbyroom/auth/command.py
+++ hobbyroom/auth/command.py
@@ -4,3 +4,8 @@ from pydantic import BaseModel, EmailStr
 class AuthorizeUser(BaseModel):
     email: EmailStr
     password: str
+
+
+class AuthorizePersona(BaseModel):
+    persona_id: str
+    user_jwt: str | None = None
