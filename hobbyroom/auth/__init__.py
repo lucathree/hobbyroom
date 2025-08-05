cef5e14059e6a2de77c7f256cfb836ae60a5f9c4
Changmin Lucas Lee <clee0627@gmail.com>
2025-08-06 05:46:54 +0900

    feat: 엔드포인트 인증 모듈에 페르소나 인증 함수 추가

--- hobbyroom/auth/__init__.py
+++ hobbyroom/auth/__init__.py
@@ -1,6 +1,13 @@
 from .adapter import AuthUnitOfWork
 from .domain import Persona, User
-from .entrypoint import user_oauth2_schema
+from .entrypoint import persona_oauth2_schema, user_oauth2_schema
 from .service import JWTHandler
 
-__all__ = ["JWTHandler", "User", "Persona", "user_oauth2_schema", "AuthUnitOfWork"]
+__all__ = [
+    "AuthUnitOfWork",
+    "JWTHandler",
+    "Persona",
+    "User",
+    "persona_oauth2_schema",
+    "user_oauth2_schema",
+]
