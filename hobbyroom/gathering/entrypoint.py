cce3cfad1c6b57ac7d699991ebb1bddcf246d4fb
Changmin Lucas Lee <clee0627@gmail.com>
2025-08-21 08:34:54 +0900

    feat: 게시물 조회 엔드포인트 추가

--- hobbyroom/gathering/entrypoint.py
+++ hobbyroom/gathering/entrypoint.py
@@ -7,7 +7,7 @@ from fastapi.routing import APIRouter
 
 from hobbyroom import auth, constants, depends, exceptions
 from hobbyroom.container import Container
-from hobbyroom.gathering import command, query, schema, service
+from hobbyroom.gathering import command, domain, query, schema, service
 
 router = APIRouter()
 
@@ -116,5 +116,34 @@ async def list_posts(
         ascending=ascending,
         page=page,
         per_page=per_page,
+    )
+    return handler.handle(qry)
+
+
+@router.get(
+    "/v1/gatherings/{gathering_id}/posts/{post_id}",
+    response_model=domain.SearchedPost,
+    status_code=http.HTTPStatus.OK,
+    summary="게시글 조회",
+    description="모임에 작성된 게시글을 조회합니다.",
+    tags=[constants.OpenApiTag.GATHERING],
+    responses=exceptions.get_responses(
+        http.HTTPStatus.UNPROCESSABLE_ENTITY,
+        http.HTTPStatus.UNAUTHORIZED,
+        http.HTTPStatus.NOT_FOUND,
+    ),
+    dependencies=[Depends(depends.get_current_persona)],
+)
+@inject
+async def retrieve_post(
+    gathering_id: UUID,
+    post_id: UUID,
+    handler: service.RetrievePostHandler = Depends(
+        Provide[Container.gathering.service.retrieve_post_handler]
+    ),
+):
+    qry = query.RetrievePost(
+        gathering_id=gathering_id,
+        post_id=post_id,
     )
     return handler.handle(qry)
