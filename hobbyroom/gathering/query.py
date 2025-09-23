2e9c9e4f5947155a67e1cdb4e50f0263d6ec49c0
Changmin Lucas Lee <clee0627@gmail.com>
2025-09-23 19:09:27 +0900

    feat: 모임 목록 조회 쿼리 추가

--- hobbyroom/gathering/query.py
+++ hobbyroom/gathering/query.py
@@ -3,6 +3,16 @@ from uuid import UUID
 from pydantic import BaseModel, Field
 
 
+class ListGatherings(BaseModel):
+    ascending: bool = False
+    page: int = Field(ge=1, default=1)
+    per_page: int = Field(le=100, default=10)
+
+    @property
+    def offset(self) -> int:
+        return (self.page - 1) * self.per_page
+
+
 class ListPosts(BaseModel):
     gathering_id: UUID
     ascending: bool
