b726893c552f81ea557b0cc5bcfc18653487ebce
Changmin Lucas Lee <clee0627@gmail.com>
2025-09-26 04:40:21 +0900

    feat: 웹소켓 채팅 엔드포인트 추가

--- hobbyroom/app/main.py
+++ hobbyroom/app/main.py
@@ -7,6 +7,7 @@ from fastapi.responses import HTMLResponse
 
 from hobbyroom import exceptions
 from hobbyroom.auth.entrypoint import router as auth_router
+from hobbyroom.chat.entrypoint import router as chat_router
 from hobbyroom.container import Container
 from hobbyroom.gathering.entrypoint import router as gathering_router
 from hobbyroom.user.entrypoint import router as user_router
@@ -37,6 +38,7 @@ def inject_dependencies(_app: FastAPI) -> None:
 
 def add_routers(_app: FastAPI) -> None:
     _app.include_router(auth_router)
+    _app.include_router(chat_router)
     _app.include_router(user_router)
     _app.include_router(gathering_router)
 
