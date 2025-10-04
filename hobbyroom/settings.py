5061155104afd03db654da391868fc0c8e351f30
Changmin Lucas Lee <clee0627@gmail.com>
2025-10-05 00:45:11 +0900

    feat: redis 패스워드 설정 추가

--- hobbyroom/settings.py
+++ hobbyroom/settings.py
@@ -8,6 +8,11 @@ class Settings(BaseSettings):
     db_name: str
     db_url: str
 
+    redis_host: str
+    redis_port: int
+    redis_password: str
+    redis_db: int = 0
+
     jwt_secret_key: str
     jwt_algorithm: str = "HS256"
     jwt_expiration_minutes: int = 60
@@ -21,6 +26,10 @@ class Settings(BaseSettings):
     @property
     def jwt_expiration_timedelta(self) -> pendulum.Duration:
         return pendulum.Duration(minutes=self.jwt_expiration_minutes)
+
+    @property
+    def redis_url(self) -> str:
+        return f"redis://:{self.redis_password}@{self.redis_host}:{self.redis_port}/{self.redis_db}"
 
 
 settings = Settings()
