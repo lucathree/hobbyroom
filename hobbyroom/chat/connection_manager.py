96e4a2bcab2a02b2297884d3ddcaf67f5de8979a
Changmin Lucas Lee <clee0627@gmail.com>
2025-10-09 23:44:05 +0900

    del: 연결 정보 갱신 메소드 제거

--- hobbyroom/chat/connection_manager.py
+++ hobbyroom/chat/connection_manager.py
@@ -121,13 +121,3 @@ class ConnectionManager:
             except WebSocketDisconnect:
                 logger.warning("WebSocket disconnected during broadcast")
                 continue
-
-    def refresh_connections(self) -> None:
-        active_connections = {}
-        for gathering_id, gathering_connection in self.active_connections.items():
-            gathering_connection.refresh_persona_connections()
-            if gathering_connection.has_connections:
-                active_connections[gathering_id] = gathering_connection
-
-        self.active_connections = active_connections
-        logger.info(f"Refreshed connections: {self.active_connections}")
